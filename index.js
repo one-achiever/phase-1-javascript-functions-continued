5ce7431fe15da0e60ca11ba930a3d05b28c6d7fe
linder <achie_31@yahoo.com>
2024-09-18 21:17:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun (activity ="roller-skate"){
+   return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun(activity = "bathe my dog");
+
+function mondayWork( activity = "go to the office"){
+ return `This Monday, I will ${activity}.`
+}
+mondayWork(activity = "work from home");
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("amazing"));
